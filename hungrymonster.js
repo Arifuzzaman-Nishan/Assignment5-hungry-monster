0e05410412ad2d7eb4b73ae42561cbb914b4e2f9
Nishan <arifuzzamannishan@gmail.com>
2021-02-07 07:47:08 +0600

    2nd task done:

--- hungrymonster.js
+++ hungrymonster.js
@@ -9,32 +9,43 @@ getDataFromApi = (foodName) => {
         .then(data => display(data))
 }
 
+
 //search btn
 getId("search-btn").addEventListener("click", () => {
     const searchInputText = getId("search-input-text").value;
     getDataFromApi(searchInputText);
 })
 
-display = (data)=>{
-    console.log(data);
-    const mealItems = data.meals;
-    const mealParentDiv = getId("meal-parent-div");
 
+const display = (data)=>{
+
+    console.log(data);
+   
+    let mealParentDiv = getId("meal-parent-div");
+    mealParentDiv.innerHTML = "";
+    mealParentDiv = getId("meal-parent-div");
+   
+    
+    const mealItems = data.meals;
     mealItems.forEach(mealList => {
         const mealChildDiv = document.createElement("div");
         const mealName = mealList.strMeal;
         const mealImg = mealList.strMealThumb;
        
         const mealInfo = `
-        <img class="img-fluid mb-4" src="${mealImg}" alt="">
-        <h5 class = "text-danger text-center">${mealName}</h5>
+        <img onclick = "displayMealDetail('${data}')" class="img-fluid mb-4" src="${mealImg}" alt="">
+
+        <h5 onclick = "displayMealDetail('${data}')" class = "text-danger text-center">${mealName}</h5>
         `;
 
          mealChildDiv.innerHTML = mealInfo;
          mealParentDiv.appendChild(mealChildDiv);
         
          mealChildDiv.className = "size m-5 col-md-3";
-
     });
    
+}
+
+const displayMealDetail = (m)=>{
+    console.log(m);
 }
