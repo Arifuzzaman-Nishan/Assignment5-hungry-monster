46f560aa4296a21b90f60534efc6e655a346cf22
Nishan <arifuzzamannishan@gmail.com>
2021-02-07 19:39:42 +0600

    all done

--- hungrymonster.js
+++ hungrymonster.js
@@ -9,11 +9,12 @@ getId = (id) => {
 }
 
 
-//here to get data from API
+//here to get data from API by meal name
 getDataFromApi = (foodName) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(res => res.json())
         .then(data => {
+            //here check if the input meal is available or not in api
             if(data.meals === null){
                 getId("not-found").innerText = "Sorry! This Meal is not Available";
             }
@@ -25,19 +26,22 @@ getDataFromApi = (foodName) => {
 //here the even handler for search button
 getId("search-btn").addEventListener("click", () => {
 
+    //here clear the previous inner html and store the html tag
     catchHtmlTagById("meal-details");
-    catchHtmlTagById("meal-parent-div");
+    catchHtmlTagById("meal-name-image");
 
+    //its clear the innerText
     getId("not-found").innerText="";
 
     const searchInputText = getId("search-input-text").value;
     
+    //here check if the search box is empty or not
     (searchInputText === "")?   getId("not-found").innerText = "Sorry! Please Enter a meal Name" : getDataFromApi(searchInputText);
 
 })
 
 
-//to store the HTML tag by ID
+//to store the HTML tag by ID and clear the previous html
 catchHtmlTagById = (parentId) => {
     mealParentDiv = getId(parentId);
     mealParentDiv.innerHTML = "";
@@ -45,34 +49,59 @@ catchHtmlTagById = (parentId) => {
 }
 
 
+//it display the meal information to user
+display = (mealTagInfo) => {
+
+    //create child div
+    mealChildDiv = document.createElement("div");
+    
+    //set the html template into child div
+    mealChildDiv.innerHTML = mealTagInfo;
+
+    //set the child div into the parent div
+    mealParentDiv.appendChild(mealChildDiv);
+
+    //here set the class name into child div
+    mealChildDiv.className = "m-5 col-md-3";
+
+}
+
+
 // here it store the all meal image and name from the api data
-const mealImageAndName = (data) => {
+mealImageAndName = (data) => {
 
-    console.log(data);
+    //here clear the previous inner html and store the html tag
+    catchHtmlTagById("meal-name-image");
 
-    catchHtmlTagById("meal-parent-div");
-
+    //it store the object of api data
     const mealItems = data.meals;
 
     mealItems.forEach(mealList => {
 
+         //gives the meal name
         const mealName = mealList.strMeal;
+
+         //gives the meal image link
         const mealImg = mealList.strMealThumb;
+
+        //gives the meal id
         const mealId = mealList.idMeal;
 
-        mealTagInfo = `
+        //template of html tags
+        const mealTagInfo = `
         <img onclick = "getDataByMealId('${mealId}')" class="img-fluid mb-4" src="${mealImg}" alt="">
         <h5 onclick = "getDataByMealId('${mealId}')" class = "text-danger text-center">${mealName}</h5>
         `;
 
         ///it display to the user to show the information
         display(mealTagInfo);
-        
+
     });
 
 }
 
 
+//get data form API by mealId
 getDataByMealId = (mealId) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
         .then(response => response.json())
@@ -80,19 +109,22 @@ getDataByMealId = (mealId) => {
 }
 
 
-let mealTagInfo ;
-
-
 //it store the all specific meal details
 specificMealDetails = (mealItem) => {
+
+    //gives the meal name
     const mealName = mealItem.strMeal;
+
+     //gives the meal image link
     const mealImg = mealItem.strMealThumb;
 
-     mealTagInfo = `
-        <img  class="img-fluid mb-4" src="${mealImg}" alt="">
-        <h2   class = "text-danger text-center">${mealName}</h2>
-        <h4>Ingredients</h4>
-        <ul class="list-group">
+    //template of html tags for specific meal
+     const mealTagInfo = 
+     `
+        <img class="img-fluid mb-4" src="${mealImg}" alt="">
+        <h2 class = "text-danger text-center mb-5">${mealName}</h2>
+        <h4 class="text-color">Ingredients</h4>
+        <ul class="list-group mt-4">
             <li class="list">${mealItem.strIngredient1}</li>
             <li class="list">${mealItem.strIngredient2}</li>
             <li class="list">${mealItem.strIngredient3}</li>
@@ -106,21 +138,13 @@ specificMealDetails = (mealItem) => {
         </ul>
         `;
 
+    //it store the html tag by id and clear the previous inner html
     catchHtmlTagById("meal-details");
 
+    //it display the information to user
     display(mealTagInfo);
-   
 
 }
 
 
 
-//it display the meal information to user
-display = (mealTagInfo) => {
-    mealChildDiv = document.createElement("div");
-
-    mealChildDiv.innerHTML = mealTagInfo;
-    mealParentDiv.appendChild(mealChildDiv);
-
-    mealChildDiv.className = "m-5 col-md-3";
-}
